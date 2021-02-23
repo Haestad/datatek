282db91144e2ae760b1a22292cd5f4a1e98a37fb
Haestad <54801050+Haestad@users.noreply.github.com>
2021-02-23 23:31:13 +0100

    part 5 oving 4

--- oving4/calculator.py
+++ oving4/calculator.py
@@ -1,8 +1,10 @@
 """ This module contains the Calculator class. """
+import numbers
+
 import numpy
 
 from function_operator import Function, Operator
-from containers import Queue
+from containers import Queue, Stack
 
 
 class Calculator:
@@ -24,9 +26,37 @@ class Calculator:
 
         self.output_queue = Queue()
 
+    def RPN(self):
+        """ Goes through the output queue,
+         and calculates and answer based on RPN.
+         This is achieved by using a stack to store the results,
+         and checking each item in output queue. """
+        stack = Stack()
+        while not self.output_queue.is_empty():
+            item = self.output_queue.pop()
+            if isinstance(item, numbers.Number):
+                stack.push(item)
+            elif isinstance(item, Function):
+                stack.push(item.execute(stack.pop()))
+            elif isinstance(item, Operator):
+                num2 = stack.pop()
+                num1 = stack.pop()
+                stack.push(item.execute(num1, num2))
+        return stack.pop()
+
 
 if __name__ == '__main__':
+    print('Uses the calculator methods directly:')
     calc = Calculator()
     print(calc.functions['EXP'].execute(
         calc.operators['ADD'].execute(1,
                                       calc.operators['MULTIPLY'].execute(2, 3))))
+
+    print('\nUses the output_queue and RPN()_')
+    calc.output_queue.push(1)
+    calc.output_queue.push(2)
+    calc.output_queue.push(3)
+    calc.output_queue.push(calc.operators['MULTIPLY'])
+    calc.output_queue.push(calc.operators['ADD'])
+    calc.output_queue.push(calc.functions['EXP'])
+    print(calc.RPN())
